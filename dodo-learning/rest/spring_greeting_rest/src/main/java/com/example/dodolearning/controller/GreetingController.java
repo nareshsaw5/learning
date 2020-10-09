69cc54a0447e4638dd7dfb79740abd741d6a3e59
Naresh Saw (nsaw) <nsaw@cisco.com>
2020-10-09 16:43:46 +0530

    corected annonation in the controller

--- dodo-learning/rest/spring_greeting_rest/src/main/java/com/example/dodolearning/controller/GreetingController.java
+++ dodo-learning/rest/spring_greeting_rest/src/main/java/com/example/dodolearning/controller/GreetingController.java
@@ -2,21 +2,23 @@ package com.example.dodolearning.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.dodolearning.domain.Greeting;
 
+
+
 @RestController
-//@RequestMapping("/greeting")
+@RequestMapping("/greeting")
 public class GreetingController {
 
     private static final String template = "Hello, %s";
     private AtomicLong counter = new AtomicLong();
 
-    @GetMapping("/greeting")
+    @RequestMapping(value = "", method=RequestMethod.GET)
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "User") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
