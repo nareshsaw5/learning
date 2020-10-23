1f14ef06de6cda97651ab558cfcd22477bb7544e
Naresh Saw (nsaw) <nsaw@cisco.com>
2020-10-23 13:20:39 +0530

    simple java application to read env variable passed in docker

---
+++ dodo-learning/docker_env_test_java/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    
+    public static void main(String[] arg){
+        String RABBIT_MQ_URL = System.getenv("RABBITMQ_URL");
+        System.out.println(" Rabbit MQ URL: "+RABBIT_MQ_URL);
+        while(true){
+
+        }
+    }
+}
